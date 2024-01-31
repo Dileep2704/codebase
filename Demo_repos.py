561508d43bc8d768270df7c9d8b06a752eebe6a9
Dileep2704 <Dileep2704>
2024-01-31 18:09:48 +0000

    commit

--- Demo_repos.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-print('Demo Repos')
-
-# COMMAND ----------
-
-
--- python_practice.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-limit = 10
-for i in range(limit):
-    print('Number :',i)
-
-# COMMAND ----------
-
-
